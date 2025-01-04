f4cfd13efe5f9fab8807c314d4fa6fdaf61c5da8
fedx-988 <fil.speicher@icloud.com>
2025-01-04 00:16:41 -0500

    1 more

--- AssaultingTheCube/AssaultCube/offsets.h
+++ AssaultingTheCube/AssaultCube/offsets.h
@@ -8,9 +8,10 @@ const DWORD AR_AMMO_OFFSET = 0x140;           // Offset for AR ammo
 const DWORD GRENADE_AMMO_OFFSET = 0x144;      // Offset for grenade ammo
 const DWORD DUAL_PISTOL_AMMO_OFFSET = 0x148;  // Offset for dual pistols ammo
 const DWORD ARMOUR_OFFSET = 0xF0;			  // Offset for armour
-//const DWORD PLAYER_COUNT_OFFSET = ; // Offset for player count
+const DWORD PLAYER_COUNT_OFFSET = 0x0005C434; // Offset for player count
 const DWORD X_HEAD_COORD_OFFSET = 0x4; // Offset for X Head Coord
 const DWORD Y_HEAD_COORD_OFFSET = 0x8; // Offset for Y Head Coord
 const DWORD Z_HEAD_COORD_OFFSET = 0xC; // Offset for Z Head Coord
+const DWORD LOCAL_PLAYER_OFFSET = 0x00195404; // Offset for Local Player
 
 #endif // OFFSETS_H
